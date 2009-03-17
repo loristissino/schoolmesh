01e11e136cb31211c2653abb1800d80cc19a98d2
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-03-17 15:09:43 +0000

    modified something about workplan items, started considering ranking

--- apps/frontend/modules/wpmodule/templates/_wpmoduleitems.php
+++ apps/frontend/modules/wpmodule/templates/_wpmoduleitems.php
@@ -7,8 +7,11 @@
 <?php foreach($wpmodule->getWpmoduleItems() as $wpmoduleitem): ?>
 
 	<?php if ($wpmoduleitem->getWpItemType()!=$lastwptype): ?>
+	
 	    <?php if (!$first): ?>
 			</ol>
+			<p>New</p>
+
 		<?php endif; ?>
 		<h3><?php echo $wpmoduleitem->getWpItemType() ?></h3>
 		<p><em><?php echo $wpmoduleitem->getWpItemType()->getDescription() ?></em></p>
@@ -20,13 +23,30 @@
 
 	<li><?php echo $wpmoduleitem; ?>
 	&nbsp;<a href="<?php echo url_for('wpmoduleitem/show?id='.$wpmoduleitem->getId()) ?>">Edit</a>
-	&nbsp;<a href="<?php echo url_for('wpmoduleitem/show?id='.$wpmoduleitem->getId()) ?>">Move Up</a>
-	&nbsp;<a href="<?php echo url_for('wpmoduleitem/show?id='.$wpmoduleitem->getId()) ?>">Move Down</a>	
+
+	<?php if ($wpmoduleitem->getRank()>1): ?>
+		&nbsp;<a href="<?php echo url_for('wpmoduleitem/show?id='.$wpmoduleitem->getId()) ?>">Move Up</a>
+	<?php endif; ?>
+	
+	<?php if ($wpmoduleitem->getRank()<$wpmoduleitem->getMaxRank()): ?>
+		&nbsp;<a href="<?php echo url_for('wpmoduleitem/show?id='.$wpmoduleitem->getId()) ?>">Move Down</a>
+	<?php endif; ?>
+	<?php /*
+		<ul>
+			<li>docente: <?php echo $wpmoduleitem->getWpmodule()->getSfGuardUser() ?></li>
+			<li>piano di lavoro: <?php echo $wpmoduleitem->getWpmoduleId() ?></li>
+			<li>item type: <?php echo $wpmoduleitem->getWpitemTypeId() ?></li>
+			<li>current rank: <?php echo $wpmoduleitem->getRank() ?></li>
+			<li>max rank here: <?php echo $wpmoduleitem->getMaxRank() ?></li>
+		</ul>
+	*/ ?>
 	</li>
+	
 
 <?php endforeach; ?>
 
 </ol>
+	<p>New</p>
 
 <?php endif; ?>
 
