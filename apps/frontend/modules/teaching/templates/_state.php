5384bda153e94e4e4cd7d59d84b8559fa5c15345
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-03-24 15:08:26 +0000

    modified teaching --> plansandreports

--- apps/frontend/modules/teaching/templates/_state.php
+++
@@ -1,5 +0,0 @@
-<?php if ($state!==NULL): ?>
-	<?php echo image_tag('wpfr_workflow_'.$state.$size, 'title=' . __($steps[$state]['stateDescription'])) ?>
-<?php else: ?>
-	<?php echo __("No info available") ?>
-<?php endif ?>
--- apps/frontend/modules/teaching/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Teaching</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
