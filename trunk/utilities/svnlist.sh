7fa462093c3387ad47aec01be7d62997c11eaf88
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-08-09 19:58:47 +0000

    reconf

--- trunk/utilities/svnlist.sh
+++ trunk/utilities/svnlist.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-svn status | grep ^? | grep -v 'cache' | grep -v 'lib/vendor' | grep -v 'web/uploads' | grep -v 'web/images/sources' | grep -v 'plugins' | sed 's/^?      //' #  | xargs svn add 
+svn status | grep ^? | grep -v 'cache' | grep -v 'lib/vendor' | grep -v 'web/uploads' | grep -v 'web/images/sources' | grep -v 'plugins' | grep -v 'mysymfony/lib' | sed 's/^?      //' #  | xargs svn add 
