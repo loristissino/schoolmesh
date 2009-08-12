9bf74b98766c4735eec3b642e7da8ab73c1aa2ee
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-08-12 15:58:56 +0000

    added web execution of system tasks

--- trunk/apps/frontend/modules/content/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/content/templates/indexSuccess.php
@@ -1,4 +1,3 @@
 <h1 class="topbar"><?php echo sfConfig::get('app_school_name'); ?></h1>
 
-<h2 class="logo"><?php echo image_tag('logo', 'Logo ITC Mattiussi') ?></h2>
-
+<h2 class="logo"><?php echo image_tag('logo', array('title'=>'Logo ITC Mattiussi', 'alt'=>'Logo ITC Mattiussi')) ?></h2>
