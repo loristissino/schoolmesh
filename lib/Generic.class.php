018c892cd1eb4cd1af7a36136f084914fc81422a
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-08-12 10:12:35 +0000

    added tests about apache and php configuration

--- lib/Generic.class.php
+++ lib/Generic.class.php
@@ -2,6 +2,23 @@
 class Generic{
 	
 	
+	public static function return_bytes($val)
+	{
+		$val = trim($val);
+		$last = strtolower($val[strlen($val)-1]);
+		switch($last) {
+			// The 'G' modifier is available since PHP 5.1.0
+			case 'g':
+				$val *= 1024;
+			case 'm':
+				$val *= 1024;
+			case 'k':
+				$val *= 1024;
+    }
+
+    return $val;
+	}
+	
 	public static function int_int_divide($x, $y) 
 	
 	{
