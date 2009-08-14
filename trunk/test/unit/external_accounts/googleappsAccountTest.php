82ae68a0187c9fbea9a2a46cfcbaf8354a262194
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2009-08-14 22:51:18 +0000

    worked a bit on external accounts

--- trunk/test/unit/external_accounts/googleappsAccountTest.php
+++ trunk/test/unit/external_accounts/googleappsAccountTest.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/../../bootstrap/Propel.php';
 
-$t = new lime_test(7, new lime_output_color());
+$t = new lime_test(0, new lime_output_color());
 
 $t->comment('Sample user');
 
@@ -16,4 +16,11 @@ $profile
 ->setFirstName('Charles')
 ->setLastName('Doe')
 ->save();
+
+$profile->addUserPermission('googleapps');
+
+$googleappsAccount = new GoogleappsAccount();
+$profile->addAccount($googleappsAccount);
+
+
 
