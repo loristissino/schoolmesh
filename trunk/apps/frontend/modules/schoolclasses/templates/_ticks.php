ce1f26899db5bd8cc823f95dbece6642a6cc7a7f
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-01-02 17:46:45 +0000

    added support for student evaluation

--- trunk/apps/frontend/modules/schoolclasses/templates/_ticks.php
+++ trunk/apps/frontend/modules/schoolclasses/templates/_ticks.php
@@ -1,8 +1,9 @@
 <?php use_helper('Javascript') ?>
 <?php $sits=$wpmodule_item->getStudentsSituationsAsArray(unserialize(base64_decode($ids)), $term_id)->getRawValue() ?>
-<td width="20">&nbsp;</td>
+<td width="10">&nbsp;</td>
 <td><?php echo html_entity_decode($wpmodule_item->getContent()) ?></td>
 
+<td width="20"><?php echo image_tag('loader.gif', array('style'=>'vertical-align: middle; display: none', 'id'=>'loader'. $wpmodule_item->getId())) ?></td>
 <?php foreach ($students as $student): ?>
 <td>
 	<ul class="sf_admin_td_actions">
@@ -10,24 +11,30 @@
 		<?php echo link_to_remote('', array(
 					'update'   => 'ticks_' . $wpmodule_item->getId(),
 					'url'      => url_for('schoolclasses/tickit?student=' . $student->getId() . '&item=' . $wpmodule_item->getId() . '&ids=' . $ids),
-					'loading'=>'$(\'loader'.$student->getId() . '_' . $wpmodule_item->getId() . '\').show();'),
+					'loading'=>'$(\'loader'. $wpmodule_item->getId() . '\').show();'),
 					array(
-					'title'=>$student->getProfile()->getFullName()
+					'title'=>$student->getProfile()->getFullName() . ' - ' . 
+						(
+						in_array($student->getId(), $sits)? 
+							format_number_choice(__('[0]currently selected|[1]currently selected'), null, $student->getProfile()->getIsMale())
+							:
+							format_number_choice(__('[0]currently not selected|[1]currently not selected'), null, $student->getProfile()->getIsMale())
+						)
 					)
 				) ?>
-		<?php echo image_tag('loader.gif', array('style'=>'vertical-align: middle; display: none', 'id'=>'loader'.$student->getId() . '_' . $wpmodule_item->getId())) ?>
 		</li>
 	</ul>
 </td>
 <?php endforeach ?>
 <td>
 	<ul class="sf_admin_td_actions">
-		<li class="sf_admin_action_<?php echo (true)? 'flag_red': 'reset' ?>">
+		<li class="sf_admin_action_grouptoggle">
 		<?php echo link_to_remote('', array(
 					'update'   =>'ticks_' . $wpmodule_item->getId(),
-					'url'      => 'schoolclasses/tickit'),
+					'url'      => url_for('schoolclasses/tickit?student=all&item=' . $wpmodule_item->getId() . '&ids=' . $ids),
+					'loading'=>'$(\'loader'.$wpmodule_item->getId() . '\').show();'),
 					array(
-					'title' => 'All selected students'
+					'title' => __('All selected students')
 					)
 				) ?>
 		</li>
