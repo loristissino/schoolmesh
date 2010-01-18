2c7079508588d0cc6ff76a8837edac01cdff4c9b
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-01-18 20:00:29 +0000

    improved change password feature

--- trunk/lib/form/ChangePasswordForm.class.php
+++ trunk/lib/form/ChangePasswordForm.class.php
@@ -5,6 +5,7 @@
           {
 			
             $this->setWidgets(array(
+			  'current_password' => new sfWidgetFormInputPassword(array(), array('size'=>25)),
 			  'password' => new sfWidgetFormInputPassword(array(), array('size'=>25)),
 			  'password_again' => new sfWidgetFormInputPassword(array(), array('size'=>25)),
 			  'type'  => new sfWidgetFormInputText(array('type'=>'hidden', 'is_hidden'=>true)),
@@ -13,6 +14,7 @@
 			$this->widgetSchema->setNameFormat('userinfo[%s]');
 			
 			$this->setValidators(array(
+				'current_password' => new sfValidatorString(),
 				'password' => new sfValidatorString(array('min_length'=>7), array('min_length' => 'The password must be of 7 characters (minimum)')),
 				'password_again' => new sfValidatorString(array('min_length'=>7), array('min_length' => 'The password must be of 7 characters (minimum)')),
 				'type' => new sfValidatorString(),
