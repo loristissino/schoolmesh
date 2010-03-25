fdb7946b7e7635f75b55420cee049e0f1ac146c7
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-03-25 14:15:57 +0000

    fixed some little bugs

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+    This file is part of SchoolMesh.
+
+    SchoolMesh is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SchoolMesh, in the web/license directory.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+*/
+
+
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
