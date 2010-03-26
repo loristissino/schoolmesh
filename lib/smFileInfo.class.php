a81cb7be18df6ea134c7bc67ad85a830cf2bda90
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-03-26 07:22:06 +0000

    partially refactored file delivery

--- lib/smFileInfo.class.php
+++ lib/smFileInfo.class.php
@@ -17,4 +17,20 @@ class smFileInfo extends SPLFileInfo
     return $result[0];
   }
   
+  
+  public function prepareDelivery(sfWebResponse $response)
+  {
+		$response->setHttpHeader('Pragma', '');
+		$response->setHttpHeader('Cache-Control', '');
+		$response->setHttpHeader('Content-Length', $this->getSize());
+		$response->setHttpHeader('Content-Type', $this->getMimeType());
+		$response->setHttpHeader('Content-Disposition', 'attachment; filename="' . html_entity_decode($this->getFilename(), ENT_QUOTES, 'UTF-8') . '"');
+
+		$tmpfile=fopen($this->getPathName(), 'r');
+
+		$response->setContent(fread($tmpfile, $this->getSize()));
+    fclose($tmpfile);
+  
+  }
+  
 }
