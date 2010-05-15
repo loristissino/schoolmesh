5658fe076744e445fc6d89ac780dd62b9ce2c567
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-05-15 07:12:08 +0000

    added moodle quasi-SSO

--- apps/frontend/modules/profile/templates/pollSuccess.php
+++ apps/frontend/modules/profile/templates/pollSuccess.php
@@ -1,10 +1,13 @@
 <h1>Compila il questionario 2010</h1>
 
-<p>Dopo aver fatto clic sul pulsante qui sotto, dovrai entrare nel corso (<em>Attività generiche...</em>) e selezionare il Questionario (in alto a sinistra)</p>
+<p>Dopo aver seguito il link qui sotto, dovrai entrare nel corso (<em>Attività generiche...</em>) e selezionare il Questionario (in alto a sinistra).</p>
 
-<form action="http://www.mattiussilab.net/moodle/login/index.php" method="post" id="login">
-<input type="hidden" name="username" id="username" size="15" value="<?php echo $sf_user->getUsername() ?>" />
-<input type="hidden" name="password" id="password" size="15" value="<?php echo $sf_user->getProfile()->getFakePassword() ?>" />
-<input type="submit" value="Login" />
-<input type="hidden" name="testcookies" value="1" />
-</form>
+<h2><?php echo __('Actions') ?></h2>
+
+<ul class="sf_admin_actions">
+<li  class="sf_admin_action_moodle"><?php echo link_to(
+  __('Moodle e-learning platform'),
+  $url
+  )
+?></li>
+</ul>
