bc2274a5ff49b913fbed6b4c7d782f7083be2e02
loris.tissino <loris.tissino@1593355a-d838-11dd-9e03-45b345501c35>
2010-05-25 06:26:30 +0000

    added focus on username field for signin page

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,7 +1,7 @@
 <h1 class="topbar"><?php echo sfConfig::get('app_school_name'); ?></h1>
 <p><?php echo __("You need to login to access the page you requested.") ?></p>
 
-<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post" id="loginbox">
 	<table>
     <?php echo $form ?>
   </table>
@@ -10,3 +10,5 @@
   <input type="submit" value="sign in" />
   <!--<a href="<?php echo url_for('@sf_guard_password') ?>"><?php echo __("Forgot your password?") ?></a>-->
 </form>
+
+<?php use_javascript('signin.js') ?>
