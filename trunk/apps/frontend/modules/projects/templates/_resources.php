84ce8d838f8ae38569e98fe1207a4eeb7ac300f5
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-09-08 16:00:10 +0000

    added treeview demo

--- trunk/apps/frontend/modules/projects/templates/_resources.php
+++ trunk/apps/frontend/modules/projects/templates/_resources.php
@@ -1,6 +1,6 @@
 <?php if (sizeof($resources)>0): ?>
 
-<h3><?php echo __('Resources') ?></h3>
+<h3><?php echo __('Resources and schedule') ?></h3>
 
 <ol>
 <?php foreach($resources as $resource): ?>
@@ -8,6 +8,7 @@
 	<li><?php echo $resource->getDescription() ?>
 	<ul>
 		<li><?php echo __('Resource type') ?>: <strong><?php echo $resource->getProjResourceType() ?></strong></li>
+		<li><?php echo __('Charged user') ?>: <strong><?php echo $resource->getChargedUserProfile() ?></strong></li>
     <?php if ($resource->getProjResourceTypeId()): ?>
       <li><?php echo __('Estimation') ?>: <strong><?php echo $resource->getProjResourceType()->getMeasurementUnit() ?>&nbsp;<?php echo $resource->getQuantityEstimated() ?></strong></li>
       <?php if($resource->getQuantityApproved()>0): ?>
