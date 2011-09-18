11fe24c6d2489bd46e061f67a5c9bdbce6444aa6
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-09-18 17:15:41 +0000

    fixed little problem in module import, and changed implementation of help index

--- trunk/apps/frontend/modules/help/actions/components.class.php
+++ trunk/apps/frontend/modules/help/actions/components.class.php
@@ -6,25 +6,54 @@ class helpComponents extends sfComponents
   {
     $this->module=$this->sf_user->getFlash('helpmodule', $this->module);
     $this->action=$this->sf_user->getFlash('helpaction', $this->action);
-    $this->config=sfYaml::load(sfConfig::get('app_config_help_index', false));
-    if(array_key_exists($this->module, $this->config))
-    {
-      if(array_key_exists($this->action, $this->config[$this->module]))
-      {
-        $this->helplink=$this->config[$this->module][$this->action];
-      }
-      elseif(array_key_exists('default', $this->config[$this->module]))
-      {
-        $this->helplink=$this->config[$this->module]['default'];
-      }
-      else
-      {
-        return sfView::NONE;
-      }
-    }
-    else
+    $this->help=sfYaml::load(sfConfig::get('app_config_help_index', false));
+    if(!is_array($this->help))
     {
       return sfView::NONE;
     }
+    
+    $modules=$this->help['help']['modules'];
+    
+    if(array_key_exists($this->module, $modules))
+    {
+      if(array_key_exists($this->action, $modules[$this->module]))
+      {
+        // we have both module and action
+        $this->helplink=$modules[$this->module][$this->action];
+      }
+      elseif(array_key_exists('default', $modules[$this->module]))
+      {
+        // we have the module and we use the default action
+        $this->helplink=$modules[$this->module]['default'];
+      }
+      else
+      {
+        // we have the module, no action, and no default is specified
+        $this->helplink=false;
+      }
+    }
+    elseif (array_key_exists('default', $modules))
+    {
+      // we use the super default 
+      $this->helplink=$modules['default'];
+    }
+    else
+    {
+      // we don't have any super default
+      $this->helplink=false;
+    }
+    
+    if(!$this->helplink)
+    {
+      return sfView::NONE;
+    }
+    
+    if(substr($this->helplink, 0, 4)!='http')
+    {
+      $this->helplink=$this->help['help']['basehref'] . $this->helplink;
+
+    }
+    
+    
   }
 }
