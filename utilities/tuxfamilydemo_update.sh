020a62b016de8f1aff1f71f9049fcd5fdd1e7660
loris.tissino <loris.tissino@gmail.com>
2011-11-13 16:15:44 +0000

    added symlink resetting for tuxfamily update script

--- utilities/tuxfamilydemo_update.sh
+++ utilities/tuxfamilydemo_update.sh
@@ -20,4 +20,12 @@ for DIR in account  email  filter  form  helper  model  schoolmesh  task  test
 
 STARTDIR=/home/schoolmesh/schoolmeshdemo.tuxfamily.org-web/htdocs
 cd "$STARTDIR"
+echo "Updating $STARTDIR"
 svn update
+
+# we need to reset the symlinks
+cd /home/
+ln -sfv ../php-include/plugins/sfJqueryReloadedPlugin/web/ sfJqueryReloadedPlugin
+ln -sfv ../php-include/plugins/sfOdfPlugin/web/ sfOdfPlugin
+ln -sfv ../php-include/lib/vendor/symfony/lib/plugins/sfPropelPlugin/web/ sfPropelPlugin
+ln -sfv ../php-include/plugins/sfTCPDFPlugin/web/ sfTCPDFPlugin
