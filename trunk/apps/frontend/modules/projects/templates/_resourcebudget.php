3a939c438617ac5f46a15fd1623183d82bbbebfe
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-11-23 12:11:58 +0000

    added print.css

--- trunk/apps/frontend/modules/projects/templates/_resourcebudget.php
+++ trunk/apps/frontend/modules/projects/templates/_resourcebudget.php
@@ -1,12 +1,12 @@
 <td style="<?php if(!$resource->getIsMonetary()) echo "color:gray" ?>"><?php echo $resource->getDescription() ?></td>
 <td><?php echo $resource->getChargedUserProfile() ?></td>
-<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>"><?php echo $resource->getProjResourceType()->getMeasurementUnit() ?></td>
-<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>"><?php echo quantityvalue($resource->getQuantityEstimated()) ?></td>
-<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>"><?php echo quantityvalue($resource->getQuantityApproved()) ?></td>
-<td style="text-align: right"><?php echo quantityvalue($resource->getStandardCost(), sfConfig::get('app_config_currency_symbol') .'/' . $resource->getProjResourceType()->getMeasurementUnit()) ?></td>
-<td style="text-align: right; font-style: italic"><?php echo currencyvalue($value) ?></td>
-<td style="text-align: right; font-style: italic"><span title="<?php echo $resource->getFinancingNotes() ?>"><?php echo currencyvalue($external) ?></span><?php if(($resource->getFinancingNotes()=='') && ($external)):?><?php echo image_tag('dubious', array('title'=>__('External financing undefined'), 'size'=>'16x16')) ?><?php endif ?></td>
-<td>
+<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>" class='amount'><?php echo $resource->getProjResourceType()->getMeasurementUnit() ?></td>
+<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>" class='amount'><?php echo quantityvalue($resource->getQuantityEstimated()) ?></td>
+<td style="text-align: right<?php if(!$resource->getIsMonetary()) echo "; color:gray" ?>" class='amount'><?php echo quantityvalue($resource->getQuantityApproved()) ?></td>
+<td style="text-align: right" class='amount'><?php echo quantityvalue($resource->getStandardCost(), sfConfig::get('app_config_currency_symbol') .'/' . $resource->getProjResourceType()->getMeasurementUnit()) ?></td>
+<td style="text-align: right; font-style: italic" class='amount'><?php echo currencyvalue($value) ?></td>
+<td style="text-align: right; font-style: italic" class='amount'><span title="<?php echo $resource->getFinancingNotes() ?>"><?php echo currencyvalue($external) ?></span><?php if(($resource->getFinancingNotes()=='') && ($external)):?><?php echo image_tag('dubious', array('title'=>__('External financing undefined'), 'size'=>'16x16')) ?><?php endif ?></td>
+<td class='actionscolumn'>
 <ul class="sf_admin_td_actions">
 	<li class="sf_admin_action_edit">
 		<?php echo link_to(__('Edit'),
