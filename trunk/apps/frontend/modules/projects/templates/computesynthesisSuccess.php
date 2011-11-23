74624f5f0532331413b089068c768d19ec1726b4
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-11-23 12:24:49 +0000

    removed non-monetary items from synthesis budget

--- trunk/apps/frontend/modules/projects/templates/computesynthesisSuccess.php
+++ trunk/apps/frontend/modules/projects/templates/computesynthesisSuccess.php
@@ -16,16 +16,16 @@
   <thead>
     <tr>
       <th class="sf_admin_text"><?php echo __('Project') ?></th>
-    <?php foreach($types as $type): ?>
-	<th width="10" style="text-align: right"><?php echo image_tag('vertical.php?text='. urlencode($type->getDescription()) .
-	'&backcolor=255-255-255&textcolor=0-0-0&ywidth=250',
-			array(
-				'alt' => $type->getDescription(),
-				'title' => $type->getDescription(),
-        'size' => '20x250')
-				)
-			?></th>
-    <?php endforeach ?>
+    <?php foreach($types as $type): if($type->getIsMonetary()):?>
+      <th width="10" style="text-align: right"><?php echo image_tag('vertical.php?text='. urlencode($type->getDescription()) .
+      '&backcolor=255-255-255&textcolor=0-0-0&ywidth=250',
+          array(
+            'alt' => $type->getDescription(),
+            'title' => $type->getDescription(),
+            'size' => '20x250')
+            )
+          ?></th>
+    <?php endif; endforeach ?>
 	<th width="10" style="text-align: right"><?php echo image_tag('vertical.php?text='. urlencode(__('Total amount')) .
 	'&backcolor=0-0-0&textcolor=255-255-63&ywidth=250',
 			array(
@@ -48,15 +48,15 @@
           )
         ?>
       </td>
-      <?php foreach($types as $type): ?>
-        <td style="text-align: right"  class='amount'>
-        <?php if(array_key_exists($type->getId(), $project['resources']->getRawValue())): ?>
-          <?php echo currencyvalue($project['resources'][$type->getId()]) ?>
-          <?php $total+=$project['resources'][$type->getId()] ?>
-          <?php @$totals_by_type[$type->getId()]+=$project['resources'][$type->getId()] ?>
-        <?php endif ?>
-        </td>
-      <?php endforeach ?>
+      <?php foreach($types as $type): if($type->getIsMonetary()):?>
+          <td style="text-align: right"  class='amount'>
+          <?php if(array_key_exists($type->getId(), $project['resources']->getRawValue())): ?>
+            <?php echo currencyvalue($project['resources'][$type->getId()]) ?>
+            <?php $total+=$project['resources'][$type->getId()] ?>
+            <?php @$totals_by_type[$type->getId()]+=$project['resources'][$type->getId()] ?>
+          <?php endif ?>
+          </td>
+      <?php endif; endforeach ?>
         <th style="text-align: right; font-weight: bold"  class='amount'>
           <?php echo currencyvalue($total) ?>
           <?php $grandtotal+=$total ?>
@@ -65,9 +65,9 @@
     <?php endforeach ?>
     <tr>
     <th><?php echo __('Grand totals') ?></th>
-    <?php foreach($types as $type): ?>
+    <?php foreach($types as $type): if ($type->getIsMonetary()): ?>
     <th style="text-align: right" class='amount'><?php echo currencyvalue($totals_by_type[$type->getId()]) ?></th>
-    <?php endforeach ?>
+    <?php endif; endforeach ?>
     <td style="text-align: right; font-weight: bold; color:blue" class='amount'><?php echo currencyvalue($grandtotal) ?></td>
     </tr>
   </tbody>
