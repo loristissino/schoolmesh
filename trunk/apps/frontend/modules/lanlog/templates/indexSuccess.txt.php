45c42df59e233b026e30f60b70a8d1c32e1c4b7c
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-11-28 14:25:10 +0000

    fixed difference layouts management and whosonline action

--- trunk/apps/frontend/modules/lanlog/templates/indexSuccess.txt.php
+++ trunk/apps/frontend/modules/lanlog/templates/indexSuccess.txt.php
@@ -1,7 +1,3 @@
 <?php foreach ($lanlog_list as $lanlog): ?>
-<?php echo sprintf("%s\t%s\t%s\t%s\n", 
-    str_pad($lanlog->getsfGuardUser()->getUsername(), 20),
-    str_pad($lanlog->getWorkstation(), 20),
-    str_pad($lanlog->getWorkstation()->getSubnet(), 20),
-    $lanlog->getUpdatedAt()) ?>
+<?php echo $lanlog->getsfGuardUser()->getUsername() ?>
 <?php endforeach; ?>
