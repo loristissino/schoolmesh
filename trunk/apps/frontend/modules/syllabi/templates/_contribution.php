d98e64f2da21c3aa31ef94f3f82dc78e642740ac
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-12-17 10:49:32 +0000

    fixed little regression

--- trunk/apps/frontend/modules/syllabi/templates/_contribution.php
+++ trunk/apps/frontend/modules/syllabi/templates/_contribution.php
@@ -1,7 +1,7 @@
 <?php echo image_tag(
   $contribution['contribution']==WpmoduleSyllabusItemPeer::PARTIAL_CONTRIBUTION ? 'partialcontribution' : 'focussedcontribution',
   array(
-    'title'=>$contribution['title']. ($appointment->getTeamId()? sprintf(' (%s)', __('this appointment is set only for some of the students')): ''),
+    'title'=>$contribution['title']. (isset($appointment)?($appointment->getTeamId()? sprintf(' (%s)', __('this appointment is set only for some of the students')): ''):''),
     'size'=>'16x16',
     )
   )
