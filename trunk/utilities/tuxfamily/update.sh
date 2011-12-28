8bc0393ab35b8841f5bd1c80e6a9bbeb0452c703
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2011-12-28 08:40:41 +0000

    edited odf plugin

--- trunk/utilities/tuxfamily/update.sh
+++ trunk/utilities/tuxfamily/update.sh
@@ -28,6 +28,7 @@ ln -sfv ../php-include/plugins/sfJqueryReloadedPlugin/web/ sfJqueryReloadedPlugi
 ln -sfv ../php-include/plugins/sfOdfPlugin/web/ sfOdfPlugin
 ln -sfv ../php-include/lib/vendor/symfony/lib/plugins/sfPropelPlugin/web/ sfPropelPlugin
 ln -sfv ../php-include/plugins/sfTCPDFPlugin/web/ sfTCPDFPlugin
+ln -sfv ../php-include/lib/vendor/symfony/data/web/sf/ sf
 
 cd images
 for FILE in default delete desc edit error first last list new next previous tick
