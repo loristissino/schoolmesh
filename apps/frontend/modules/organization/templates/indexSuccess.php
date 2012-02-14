65aa9b6841dfd6c94a2128005bb3d511a121af3e
loris.tissino <loris.tissino@gmail.com>
2012-02-14 19:30:42 +0000

    fixed few issues in organization module

--- apps/frontend/modules/organization/templates/indexSuccess.php
+++ apps/frontend/modules/organization/templates/indexSuccess.php
@@ -12,7 +12,7 @@
     <tr>
       <th class="sf_admin_text"><?php echo __('Key role') ?></th>
       <th class="sf_admin_text"><?php echo __('Quality code') ?></th>
-      <th class="sf_admin_text"><?php echo __('Name') ?></th>
+      <th class="sf_admin_text"><?php echo __('Charged user') ?></th>
       <th class="sf_admin_text"><?php echo __('Expiry') ?></th>
       <th class="sf_admin_text"><?php echo __('Team') ?></th>
     </tr>
@@ -20,15 +20,23 @@
   <tbody>
 	<?php $i=0 ?>
     <?php foreach($list as $item): ?>
-      <?php foreach($item['userteam'] as $component): ?>
-        <tr class="sf_admin_row <?php echo (++$i & 1)? 'odd':'even' ?>">
-          <td><?php echo $item['keyrole']->getMaleDescription() ?></td>
-          <td><?php echo $item['keyrole']->getQualityCode() ?></td>
-          <td><?php echo $component->getSfGuardUser()->getProfile()->getFullname() ?></td>
-          <td style="text-align: right"><?php include_partial('content/expiry', array('date'=>$component->getExpiry('U'))) ?></td>
-          <td><?php echo link_to_if($sf_user->hasCredential('teams'), $component->getTeam(), url_for('teams/show?id=' . $component->getTeam()->getId())) ?></td>
-        </tr>
-      <?php endforeach ?>
+      <?php if($item['userteam']): ?>
+        <?php foreach($item['userteam'] as $component): ?>
+          <tr class="sf_admin_row <?php echo (++$i & 1)? 'odd':'even' ?>">
+            <td><?php echo $item['keyrole']->getMaleDescription() ?></td>
+            <td><?php echo $item['keyrole']->getQualityCode() ?></td>
+            <td><?php echo link_to_if($sf_user->hasCredential('admin'), $component->getSfGuardUser()->getProfile()->getFullname(), url_for('users/edit?id=' . $component->getSfGuardUser()->getId()))?></td>
+            <td style="text-align: right"><?php include_partial('content/expiry', array('date'=>$component->getExpiry('U'))) ?></td>
+            <td><?php echo link_to_if($sf_user->hasCredential('teams'), $component->getTeam(), url_for('teams/show?id=' . $component->getTeam()->getId())) ?></td>
+          </tr>
+        <?php endforeach ?>
+        <?php else: ?>
+          <tr class="sf_admin_row <?php echo (++$i & 1)? 'odd':'even' ?>">
+            <td class="warning"><?php echo $item['keyrole']->getMaleDescription() ?></td>
+            <td class="warning"><?php echo $item['keyrole']->getQualityCode() ?></td>
+            <td colspan="3" class="highlighted warning"><?php echo __('No one in charge') ?></td>
+          </tr>
+      <?php endif ?>
     <?php endforeach ?>
   </tbody>
 </table>
--- lib/model/Role.php
+++ lib/model/Role.php
@@ -11,13 +11,20 @@
 
 class Role extends BaseRole
 {
-    public function __toString()
-    {
-        return $this->getMaleDescription();
-    }
+  public function __toString()
+  {
+      return $this->getMaleDescription();
+  }
 	
 	public function getRoleDescriptionByGender($isMale=true)
 	{
 		return ($isMale? $this->getMaleDescription(): $this->getFemaleDescription());
 	}
+  
+  public function getUsersPlayingRole()
+  {
+     $userteam=RolePeer::retrieveUsersPlayingRole($this);
+     if($this->getQualityCode()=='REDP') Generic::logMessage('get', $userteam);
+     return sizeof($userteam) ? $userteam : false;
+  }
 }
