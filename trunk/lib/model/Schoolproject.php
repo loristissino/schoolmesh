40f42597ea0f0bd7817191399e3c1c1c7ea64d0c
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-02-17 18:10:15 +0000

    changed project charges letter generation, removing references to non human (internal) resources

--- trunk/lib/model/Schoolproject.php
+++ trunk/lib/model/Schoolproject.php
@@ -380,7 +380,6 @@ class Schoolproject extends BaseSchoolproject {
       $criteria=new Criteria();
     }
     $criteria->addJoin(ProjResourcePeer::PROJ_RESOURCE_TYPE_ID, ProjResourceTypePeer::ID);
-		$criteria->addAscendingOrderByColumn(ProjResourcePeer::SCHEDULED_DEADLINE);
 		return parent::getProjResources($criteria);
 	}
 
