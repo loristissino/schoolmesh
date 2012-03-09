8bca87182257dd16c1d286670835ab4dc231f1b0
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-03-09 20:40:02 +0000

    added template for lanlog list (text)

---
+++ trunk/apps/frontend/modules/lanlog/templates/listSuccess.txt.php
@@ -0,0 +1,9 @@
+<?php foreach ($lanlog_list as $lanlog): ?>
+<?php echo sprintf("%s:%s:%s:%s:%s\n",
+  $lanlog->getsfGuardUser()->getUsername(),
+  $lanlog->getsfGuardUser()->getProfile()->getFullname(),
+  $lanlog->getWorkstation(),
+  $lanlog->getCreatedAt('d/m/y H.i.s'),
+  $lanlog->getUpdatedAt('d/m/y H.i.s')
+  ) ?>
+<?php endforeach; ?>
