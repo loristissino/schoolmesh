7d0d91def92a378a0b25f4194239d283078ea9e1
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-04-09 17:58:17 +0000

    worked on some issues concerning i18n

---
+++ trunk/apps/webdev/modules/translations/templates/xliffSuccess.php
@@ -0,0 +1,14 @@
+<?xml version="1.0"?>
+<xliff version="1.0">
+  <file source-language="EN" target-language="<?php echo $lang ?>" datatype="plaintext" original="messages" date="2009-07-23T14:10:48Z" product-name="messages">
+  <!-- localization automatically generated with http://translate.google.com -->
+    <body>
+<?php foreach($units as $unit): ?>
+      <trans-unit id="<?php echo $unit['id'] ?>">
+        <source><?php echo $unit['source'] ?></source>
+        <target><?php echo $unit['machine'] ?></target>
+      </trans-unit>
+<?php endforeach ?>
+     </body>
+  </file> 
+</xliff>
