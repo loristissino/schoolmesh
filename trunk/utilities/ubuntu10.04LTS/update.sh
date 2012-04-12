2b287a7aadd67abcad60792905d8c52b5c6409a5
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-04-12 13:48:57 +0000

    changed notes file

--- trunk/utilities/ubuntu10.04LTS/update.sh
+++ trunk/utilities/ubuntu10.04LTS/update.sh
@@ -10,7 +10,12 @@ for DIR in apps bin config data doc error graph plugins templates utilities web
     svn update
   done
 
-rm -rf "$STARTDIR/utilities/ubuntu10.04LTS/setup.sh" # we remove this file because it is dangerous
+# we remove these files because they are dangerous
+rm -rf "$STARTDIR/utilities/ubuntu10.04LTS/setup.sh"
+rm -rf "$STARTDIR/bin/schoolmesh_application_createtables"
+rm -rf "$STARTDIR/bin/schoolmesh_application_importtables"
+
+# we copy the other ones
 sudo cp -v "$STARTDIR"/bin/schoolmesh* /usr/local/bin
   
 STARTDIR="$STARTDIR/lib"
@@ -21,4 +26,7 @@ for DIR in account  email  filter  form  helper  model  schoolmesh  task  test
     svn update
   done
 
-
+cd "$STARTDIR"
+grep Release doc/notes.txt | head -1 > doc/lastrevision.txt
+echo " - svn revision: " >> doc/lastrevision.txt
+LANG=C svn info doc/notes.txt | grep Revision | cut -d: -f2 >> doc/lastrevision.txt
