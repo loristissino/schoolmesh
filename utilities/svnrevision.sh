4eab0c7110f2b3529334d712f431c7c0f849020f
loris.tissino <loris.tissino@gmail.com>
2012-04-15 09:49:37 +0000

    refactored svn version extraction

--- utilities/svnrevision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# there should be a better way - I mean, without checking the web server...
-
-cd /var/schoolmesh
-find apps lib config bin plugins web utilities doc ! -path '*.svn*' -exec  svn info {} \; 2>/dev/null| grep ^Revision: | cut -d: -f2 | sort -n | uniq | tail
-
--- utilities/ubuntu10.04LTS/update.sh
+++ utilities/ubuntu10.04LTS/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #This script is used to update the demo application on a Ubuntu10.04LTS server.
 
+ATOM=http://code.google.com/feeds/p/schoolmesh/svnchanges/basic
+
 STARTDIR=/var/schoolmesh
 
 for DIR in apps bin config data doc error graph plugins templates utilities web
@@ -29,4 +31,25 @@ for DIR in account  email  filter  form  helper  model  schoolmesh  task  test
 cd "$STARTDIR"
 grep Release doc/notes.txt | head -1 > doc/lastrevision.txt
 echo " - svn revision: " >> doc/lastrevision.txt
-LANG=C svn info doc/notes.txt | grep Revision | cut -d: -f2 >> doc/lastrevision.txt
+
+SOURCE=$(mktemp)
+XSL=$(mktemp)
+
+cat > $XSL <<EOF
+<xsl:stylesheet
+  version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:atom="http://www.w3.org/2005/Atom"> 
+<xsl:output method="text"/>
+<xsl:template match="/">
+<xsl:for-each select="atom:feed/atom:entry">
+      <xsl:value-of select="atom:id"/>
+      <xsl:text>
+</xsl:text>
+    </xsl:for-each>
+  </xsl:template>
+</xsl:stylesheet>
+EOF
+
+wget -O $SOURCE "$ATOM" 2>/dev/null
+
+xsltproc -o /dev/stdout $XSL $SOURCE | head -1 | sed -e 's|.*\/||' >> doc/lastrevision.txt
