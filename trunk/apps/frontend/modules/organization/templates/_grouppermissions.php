a7922be09be3f297539e20e16dd2fa850db51812
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-05-05 20:12:29 +0000

    added simple-list css item

--- trunk/apps/frontend/modules/organization/templates/_grouppermissions.php
+++ trunk/apps/frontend/modules/organization/templates/_grouppermissions.php
@@ -1,5 +1,5 @@
 <?php if(sizeof($grouppermissions)): ?>
-<ul>
+<ul class="simple_list">
 <?php foreach($grouppermissions as $grouppermission): $group=sfGuardGroupPeer::retrieveByPK($grouppermission->getGroupId()) ?>
   <li><?php echo link_to_if($sf_user->hasCredential('users'), $group->getName(), url_for('users/list?query=guardgroups:'.$group->getName(). ' and active:true')) ?></li>
 <?php endforeach ?>
