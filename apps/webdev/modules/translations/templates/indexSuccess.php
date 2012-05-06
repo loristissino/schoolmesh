d09fac5b04bc78aac87438492159238afb147065
loris.tissino <loris.tissino@gmail.com>
2012-05-06 16:41:23 +0000

    improved module for translations managemnt and added french

--- apps/webdev/modules/translations/templates/indexSuccess.php
+++ apps/webdev/modules/translations/templates/indexSuccess.php
@@ -1,5 +1,18 @@
 <h1>Translations</h1>
 
+<p>Instructions:</p>
+<ol>
+<li>Check the reference localization file and see that there are no duplicates in ids or items</li>
+<li>Check the other translations, one by one</li>
+</ol>
+
+<hr />
+
+<p>Reference localization file:</p>
+<ul>
+<li><?php echo link_to($reference, url_for('translations/reference?lang='. $reference)) ?></li>
+</ul>
+<p>Other available localization files:</p>
 <ul>
 <?php foreach($languages as $language): ?>
 <li><?php echo link_to($language, url_for('translations/show?lang='. $language)) ?></li>
