25772c17a2b10d9ab99ccae0b5b6bc84270b5e99
loris.tissino <loris.tissino@gmail.com>
2012-05-31 04:44:25 +0000

    enhanced organization module's interface

--- apps/frontend/modules/roles/templates/_minmaxcheck.php
+++ apps/frontend/modules/roles/templates/_minmaxcheck.php
@@ -1,6 +1,6 @@
 <?php if($Role->getMin() and $number<$Role->getMin()): ?>
-  <?php include_partial('content/dubious', array('text'=>__('Minimun number of assignees unreached'))) ?>
+  <?php include_partial('content/dubious', array('text'=>__('Minimun number of assignees (%number%) unreached', array('%number%'=>$Role->getMin())))) ?>
 <?php endif ?>
 <?php if($Role->getMax() and $number>$Role->getMax()): ?>
-  <?php include_partial('content/dubious', array('text'=>__('Maximum number of assignees exceeded'))) ?>
+  <?php include_partial('content/dubious', array('text'=>__('Maximum number of assignees (%number%) exceeded', array('%number%'=>$Role->getMax())))) ?>
 <?php endif ?>
