13a5e7f9545e52fc50d176c330b587ed5bafb2b3
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-06-19 12:59:34 +0000

    added financing notes in budget check action

--- trunk/apps/frontend/modules/projects/templates/checkbudgetcompatibilitySuccess.php
+++ trunk/apps/frontend/modules/projects/templates/checkbudgetcompatibilitySuccess.php
@@ -35,12 +35,13 @@
   <th><?php echo __('Budget') ?></th>
   <th><?php echo __('Activities') ?></th>
   <th><?php echo __('Difference') ?></th>
+  <th><?php echo __('Amount ext') ?></th>
 </tr>
-<?php $d=0 ?>
+<?php $d=0; $btotal=0; $ftotal=0 ?>
 <?php foreach($be['list'] as $rid=>$rvalue): $atotal=0 ?>
   <tr>
     <td><?php echo link_to($rvalue['description'], url_for('projects/viewresourceactivities?id=' . $rid))?></td>
-    <td style="text-align: right"><?php echo currencyvalue($rvalue['budget']) ?></td>
+    <td style="text-align: right"><?php echo currencyvalue($rvalue['budget']); $btotal+=$rvalue['budget'] ?></td>
     <td style="text-align: right">
       <?php if(isset($rvalue['activities'])): ?>
         <?php foreach($rvalue['activities'] as $aid=>$avalue): ?>
@@ -55,11 +56,17 @@
         <span style="color: <?php echo budgetcolor($amount, 0) ?>"><?php echo currencyvalue($amount) ?></span>
       <?php endif ?>
     </td>
+    <td style="text-align: right">
+      <span title="<?php echo $rvalue['financing_notes'] ?>"><?php echo currencyvalue($rvalue['amount_funded_externally']); $ftotal+=$rvalue['amount_funded_externally'] ?></span>
+    </td>
   </tr>
 <?php endforeach ?>
   <tr>
-    <th colspan="3"><?php echo __('Difference') ?></th>
+    <th><?php echo __('Total amount') ?></th>
+    <td style="text-align: right"><?php echo currencyvalue($btotal) ?></td>
+    <td></td>
     <td style="text-align: right"><span style="color: <?php echo budgetcolor($d, 0) ?>"><?php echo currencyvalue($d) ?></span></td>
+    <td style="text-align: right"><?php echo currencyvalue($ftotal) ?></td>
   </tr>
 </table>
 
