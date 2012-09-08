62d50d84fa267666be7977c919080d9ae1c2dbf2
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-09-08 12:55:30 +0000

    refactored authentication class

--- trunk/lib/schoolmesh/Authentication.class.php
+++ trunk/lib/schoolmesh/Authentication.class.php
@@ -12,63 +12,17 @@
 
 class Authentication {
 
-/*
-FIXME
-This code must be refactored. In particular, it would be nicer to have a sort of PAM-like authentication schema, maybe defining an array of authentication modes.
-*/
-
 	static function testOnly($username, $password)
 	{
     $user=sfGuardUserProfilePeer::retrieveByUsername($username);
-    $profile=$user->getProfile();
-		$profile
-		->setLastLoginAt(time())
-		->save();
-
-		return true;
+    return self::storeAuthentication($user, true);
 	}
-	
-	static function checkFirstSambaThenDBPassword($username, $password)
-	{
-    $user=sfGuardUserProfilePeer::retrieveByUsername($username);
-    $profile=$user->getProfile();
-	
-    if ($profile->hasAccountOfType('samba'))
-    {
-      $authenticationOk=self::checkSambaPassword($username, $password);
-      if ($authenticationOk)
-      {
-        $profile
-        ->setLastLoginAt(time())
-        ->save();
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
-    else
-    {
-      $authenticationOk=self::checkDBPassword($username, $password);
-      if ($authenticationOk)
-      {
-        $profile
-        ->setLastLoginAt(time())
-        ->save();
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-
-    }
-	}
-	
+  
 	static function checkDBPassword($username, $password)
 	{
 		$user=sfGuardUserProfilePeer::retrieveByUsername($username);
+    // we are always sure that the user exists, because otherwise the form validator would have failed
+    
 		$algorithm = $user->getAlgorithm();
 		if (false !== $pos = strpos($algorithm, '::'))
 		{
@@ -79,7 +33,7 @@ This code must be refactored. In particular, it would be nicer to have a sort of
 		  throw new sfException(sprintf('The algorithm callable "%s" is not callable.', $algorithm));
 		}
 
-		return $user->getPassword() == call_user_func_array($algorithm, array($user->getSalt().$password));
+    return self::storeAuthentication($user, $user->getPassword() == call_user_func_array($algorithm, array($user->getSalt().$password)));
 	}
 
 	static function checkLdapPassword($username, $password)
@@ -95,7 +49,12 @@ This code must be refactored. In particular, it would be nicer to have a sort of
 
 		ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-		return @ldap_bind($connect, $dn, $password);
+    if(@ldap_bind($connect, $dn, $password))
+    {
+      $user=sfGuardUserProfilePeer::retrieveByUsername($username);
+      return self::storeAuthentication($user, true);
+    }
+    return false;
 	}
  
 	static function checkSambaPassword($username, $password)
@@ -103,21 +62,34 @@ This code must be refactored. In particular, it would be nicer to have a sort of
 		$share = sfConfig::get('app_authentication_samba_share');
 		$address = sfConfig::get('app_authentication_samba_address');
 		
-/*		if ($realuser=ReservedUsernamePeer::retrieveByUserName($username))
-		{
-			$username=$realuser->getAliasTo();
-		}
-*/
-		
 		$command = sprintf('echo exit | smbclient "%s" "%s" -I %s -U %s', $share, $password, $address, $username);
 
 		$output="";
 		$result=-1;
 		@exec($command, $output, $result);
 		
-		
-		return ($result == 0);
+    if($result == 0)
+    {
+      $user=sfGuardUserProfilePeer::retrieveByUsername($username);
+      return self::storeAuthentication($user, true);
+    }
+    return false;
+    
 	}
+
+  static public function storeAuthentication(sfGuardUser $user, $auth=false)
+  {
+    if($auth)
+    {
+      $profile=$user->getProfile();
+      $profile
+      ->setLastLoginAt(time())
+      ->save();
+      return true;
+    }
+    return false;
+  }
+
   
   public static function generateRandomPassword()
   {
