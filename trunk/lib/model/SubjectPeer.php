774cf528e82cd8dfb23ef05115664e1d6d986b36
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-09-09 15:59:27 +0000

    fixed SubjectPeer class

--- trunk/lib/model/SubjectPeer.php
+++ trunk/lib/model/SubjectPeer.php
@@ -13,24 +13,22 @@
 class SubjectPeer extends BaseSubjectPeer
 {
 	
-	
 	static public function retrieveByShortcut($shortcut)
 	{
-	$c=new Criteria();
-	$c->add(self::SHORTCUT, $shortcut);
-	$t = parent::doSelectOne($c);
-	return $t;
-		
+    $c=new Criteria();
+    $c->add(self::SHORTCUT, $shortcut);
+    $c->add(self::IS_ACTIVE, true);
+    $t = parent::doSelectOne($c);
+    return $t;
 	}
 
 	static public function retrieveAllByRank()
 	{
-	$c=new Criteria();
-  $c->add(self::IS_ACTIVE, true);
-	$c->addAscendingOrderByColumn(SubjectPeer::RANK);
-	$t = parent::doSelect($c);
-	return $t;
-		
+    $c=new Criteria();
+    $c->add(self::IS_ACTIVE, true);
+    $c->addAscendingOrderByColumn(SubjectPeer::RANK);
+    $t = parent::doSelect($c);
+    return $t;
 	}
 
 
