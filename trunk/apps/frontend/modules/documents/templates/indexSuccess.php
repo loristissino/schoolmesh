84b00a1a6a0a53997df89e45a6e3ecf146f1375f
loris.tissino@gmail.com <loris.tissino@gmail.com@1593355a-d838-11dd-9e03-45b345501c35>
2012-11-01 12:28:55 +0000

    added info for empty documents table

--- trunk/apps/frontend/modules/documents/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/documents/templates/indexSuccess.php
@@ -5,13 +5,18 @@
 
 <?php include_partial('content/flashes'); ?>
 
+<?php if(sizeof($Doctypes)>0): ?>
 <?php foreach($Doctypes as $Doctype): ?>
   <?php include_component('documents', 'doctype', array('Doctype'=>$Doctype)) ?>
 <?php endforeach ?>
 
 <hr />
 
+<?php else: ?>
+<p><?php echo __('No documents available.') ?></p>
+<?php endif ?>
+
 <h2><?php echo __('Actions') ?></h2>
 <ul class="sf_admin_actions">
-<?php echo li_link_to_if('action_documents', true, __('View other static documents'), url_for('content/documents'), array('title'=>__('View other documents that are not managed through the documents module'))) ?>
+<?php echo li_link_to_if('action_documents', true, __('View other static documents'), url_for('content/documents'), array('title'=>__('View other documents that are not managed by Schoolmesh\'s documents module'))) ?>
 </ul>
